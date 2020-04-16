53dbe205847272304333ca60ad585a5b62aaffad
Rob Atkinson <rob@metalinkage.com.au>
2020-04-16 10:45:09 +1000

    Deprecate automatic registration of publishing signals

--- rdf_io/signals/__init__.py
+++ rdf_io/signals/__init__.py
@@ -1,4 +1,4 @@
 from rdf_io.signals.utils import *
- 
-signals.post_save.connect(setup_signals, sender=ObjectMapping)
-sync_signals()
+# originally configured signals automatically - but because this results in circular dependencies with modules that set up object mappings this is deprecated
+#signals.post_save.connect(setup_signals, sender=ObjectMapping)
+#sync_signals()
