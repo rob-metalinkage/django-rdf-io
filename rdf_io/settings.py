72596e5e06efd4c23e0f86d6cf4100380b826a08
mark doerr <mark.doerr@uni-greifswald.de>
2022-09-05 11:15:48 +0200

    fix: default_auto_field added in apps.py and settings

--- rdf_io/settings.py
+++ rdf_io/settings.py
@@ -108,4 +108,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
